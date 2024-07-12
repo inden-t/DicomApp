95cc44779006a1f3ecf4927fe1bfe40d1c080d51
inden <52695537+inden-t@users.noreply.github.com>
2024-07-12 21:35:16 +0900

    中ボタンドラッグでの画像移動を追加

--- projects/WpfApp/Views/ImageViewer.xaml.cs
+++ projects/WpfApp/Views/ImageViewer.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using DicomApp.ViewModels;
 
 namespace DicomApp.Views
@@ -8,6 +9,8 @@ namespace DicomApp.Views
     public partial class ImageViewer : UserControl
     {
         private readonly ImageViewerViewModel _viewModel;
+        private Point _lastMousePosition;
+        private bool _isScrolling;
 
         public ImageViewer(ImageViewerViewModel imageViewerViewModel)
         {
@@ -30,6 +33,46 @@ namespace DicomApp.Views
             if (_viewModel != null)
             {
                 _viewModel.UpdateViewerSize(e.NewSize.Width, e.NewSize.Height);
+            }
+        }
+
+        private void ScrollViewer_MouseDown(object sender,
+            MouseButtonEventArgs e)
+        {
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                _isScrolling = true;
+                _lastMousePosition = e.GetPosition(ImageScrollViewer);
+                ImageScrollViewer.Cursor = Cursors.SizeAll;
+                e.Handled = true;
+            }
+        }
+
+        private void ScrollViewer_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isScrolling)
+            {
+                Point currentPosition = e.GetPosition(ImageScrollViewer);
+                double deltaX = currentPosition.X - _lastMousePosition.X;
+                double deltaY = currentPosition.Y - _lastMousePosition.Y;
+
+                ImageScrollViewer.ScrollToHorizontalOffset(
+                    ImageScrollViewer.HorizontalOffset - deltaX);
+                ImageScrollViewer.ScrollToVerticalOffset(
+                    ImageScrollViewer.VerticalOffset - deltaY);
+
+                _lastMousePosition = currentPosition;
+                e.Handled = true;
+            }
+        }
+
+        private void ScrollViewer_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.MiddleButton == MouseButtonState.Released)
+            {
+                _isScrolling = false;
+                ImageScrollViewer.Cursor = Cursors.Arrow;
+                e.Handled = true;
             }
         }
     }
