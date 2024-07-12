71ba11f697f8df87c63cea340793f35b5eea6d0f
inden <52695537+inden-t@users.noreply.github.com>
2024-07-12 14:31:07 +0900

    不要になったメンバを削除

--- projects/WpfApp/Models/FileManager.cs
+++ projects/WpfApp/Models/FileManager.cs
@@ -9,8 +9,6 @@ namespace DicomApp.Models
 
         public ReactiveCollection<DICOMFile> DicomFiles { get; } = new();
 
-        public ReactiveProperty<int> SelectedIndex { get; } = new(-1);
-
         public FileManager(IImageCaches imageCaches)
         {
             _imageCaches = imageCaches;
@@ -19,46 +17,13 @@ namespace DicomApp.Models
         public void ClearFiles()
         {
             DicomFiles.Clear();
-            SelectedIndex.Value = -1;
         }
 
         public void AddFile(DICOMFile file)
         {
             DicomFiles.Add(file);
-            if (SelectedIndex.Value == -1)
-            {
-                SelectedIndex.Value = 0;
-            }
 
             _imageCaches.AddFile(file);
-        }
-
-        public void SetSelectedIndex(int index)
-        {
-            if (index >= 0 && index < DicomFiles.Count)
-            {
-                SelectedIndex.Value = index;
-            }
-        }
-
-        public void SwitchImageByOffset(int offset)
-        {
-            if (DicomFiles.Count == 0) return;
-
-            int newIndex = SelectedIndex.Value + offset;
-            newIndex = Math.Max(0, Math.Min(newIndex, DicomFiles.Count - 1));
-            SelectedIndex.Value = newIndex;
-        }
-
-        public DICOMFile GetSelectedFile()
-        {
-            if (SelectedIndex.Value >= 0 &&
-                SelectedIndex.Value < DicomFiles.Count)
-            {
-                return DicomFiles[SelectedIndex.Value];
-            }
-
-            return null;
         }
     }
 }
