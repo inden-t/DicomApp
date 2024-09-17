821af958b3306e28cfdb1270d3ef5b10ab9353fb
inden <52695537+inden-t@users.noreply.github.com>
2024-09-17 21:39:21 +0900

    血管抽出リボンタブのクラスを分離

---
+++ projects/WpfApp/Views/BloodVesselExtractionRibbonTab.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls.Ribbon;
+
+namespace DicomApp.Views
+{
+    public partial class BloodVesselExtractionRibbonTab : RibbonTab
+    {
+        public BloodVesselExtractionRibbonTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
