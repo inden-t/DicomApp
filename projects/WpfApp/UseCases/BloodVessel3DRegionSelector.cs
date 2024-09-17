62f047c8da49df676abbc39dd765b7760ca55e72
inden <52695537+inden-t@users.noreply.github.com>
2024-09-17 23:03:32 +0900

    BloodVessel3DRegionSelectorをnamespaceに入れた

--- projects/WpfApp/UseCases/BloodVessel3DRegionSelector.cs
+++ projects/WpfApp/UseCases/BloodVessel3DRegionSelector.cs
@@ -1,24 +1,27 @@
 ﻿using System.Windows.Media.Media3D;
 using DicomApp.Models;
 
-public class BloodVessel3DRegionSelector
+namespace DicomApp.UseCases
 {
-    // 3D塗りつぶし選択の実装
-    public void Select3DRegion(Point3D seedPoint, int threshold)
+    public class BloodVessel3DRegionSelector
     {
-        // 3D領域選択のロジックを実装
-    }
+        // 3D塗りつぶし選択の実装
+        public void Select3DRegion(Point3D seedPoint, int threshold)
+        {
+            // 3D領域選択のロジックを実装
+        }
 
-    // 選択領域の編集機能
-    public void EditRegion(Point3D point, bool isAdd)
-    {
-        // 領域の追加または削除のロジックを実装
-    }
+        // 選択領域の編集機能
+        public void EditRegion(Point3D point, bool isAdd)
+        {
+            // 領域の追加または削除のロジックを実装
+        }
 
-    // 選択領域の取得
-    public BloodVessel3DRegion GetSelectedRegion()
-    {
-        // 選択された領域を返す
-        return null;
+        // 選択領域の取得
+        public BloodVessel3DRegion GetSelectedRegion()
+        {
+            // 選択された領域を返す
+            return null;
+        }
     }
 }
