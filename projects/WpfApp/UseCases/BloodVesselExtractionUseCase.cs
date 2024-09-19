16e3654a327b757981038753c1f686eacb4cec76
inden <52695537+inden-t@users.noreply.github.com>
2024-09-19 15:04:41 +0900

    モデル保存処理の大枠を実装

--- projects/WpfApp/UseCases/BloodVesselExtractionUseCase.cs
+++ projects/WpfApp/UseCases/BloodVesselExtractionUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Media.Media3D;
 using DicomApp.Models;
 using DicomApp.UseCases;
 
@@ -66,5 +67,71 @@ public class BloodVesselExtractionUseCase
 
     public async Task SaveModel()
     {
+        IProgressWindow progressWindow = _progressWindowFactory.Create();
+        progressWindow.SetWindowTitle("モデル保存中");
+        progressWindow.Start();
+        progressWindow.SetStatusText("サーフェスモデルを保存中...");
+
+        try
+        {
+            // ファイル保存ダイアログを表示
+            var saveFileDialog = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter =
+                    "3D Model Files (*.stl, *.obj, *.ply)|*.stl;*.obj;*.ply",
+                DefaultExt = ".stl"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                var selectedFile = saveFileDialog.FileName;
+                var region = _regionSelector.GetSelectedRegion();
+                var model3DGroup = await _modelGenerator.GenerateModelAsync(
+                    _fileManager, region, 220, new Progress<(int, string)>());
+
+                // 選択されたファイル形式に応じて保存処理を行う
+                if (selectedFile.EndsWith(".stl"))
+                {
+                    await SaveSTLModel(selectedFile, model3DGroup);
+                }
+                else if (selectedFile.EndsWith(".obj"))
+                {
+                    await SaveOBJModel(selectedFile, model3DGroup);
+                }
+                else if (selectedFile.EndsWith(".ply"))
+                {
+                    await SavePLYModel(selectedFile, model3DGroup);
+                }
+
+                MessageBox.Show($"モデルを {selectedFile} に保存しました。", "保存完了",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show($"モデルの保存中にエラーが発生しました: {ex.Message}", "エラー",
+                MessageBoxButton.OK, MessageBoxImage.Error);
+            Console.WriteLine($"詳細なエラー情報: {ex}");
+        }
+        finally
+        {
+            progressWindow.End();
+        }
+    }
+
+    private async Task SaveSTLModel(string filePath, Model3DGroup model3DGroup)
+    {
+        // STLファイルの保存処理を実装
+        // 例: https://github.com/ericnewton76/gmf
+    }
+
+    private async Task SaveOBJModel(string filePath, Model3DGroup model3DGroup)
+    {
+        // OBJファイルの保存処理を実装
+    }
+
+    private async Task SavePLYModel(string filePath, Model3DGroup model3DGroup)
+    {
+        // PLYファイルの保存処理を実装
     }
 }
